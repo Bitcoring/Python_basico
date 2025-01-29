77bfe2c53012c6ec99d99d89e82747f1f53e30ff
Hammer <bitcoring@gmail.com>
2025-01-29 20:10:30 +0100

    cambios_ejercicio

--- 04_strings.py
+++ 04_strings.py
@@ -1,11 +1,19 @@
-"""
-CADENAS DE TEXTO  =>  Strings
+# """
+# CADENAS DE TEXTO  =>  Strings
 
-"""
+# """
 
-string_1 = 'hola'
+# string_1 = 'hola'
 
-string_2 = "hola"
+# string_2 = "hola"
 
-string_3 = """hola"""
+# string_3 = """hola"""
+
+nombre = "Hammer"
+
+apellido = "arandia"
+
+edad = 35
+
+print(f"hola me llamo {nombre} {apellido} y tengo {edad} años")
 
